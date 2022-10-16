8e9d1311caf948ab08f0cfdfbc3081cf446ae0a0
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2022-10-16 19:58:20 +0300

    Add files via upload

---
+++ 10 ile 200 arasındaki tam sayılardan 3 katının 2 fazlası 5 ile tam bölünebilen sayıları gösteren program.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+main(){
+setlocale(LC_ALL,"Turkish");
+int i=0;
+int sayi;
+cout<<"Programa Hoþgeldiniz...";
+while (i<=3){
+
+	cout<<endl<<"Girmek Ýstediðiniz Sayýyý Giriniz: ";
+	cin>>sayi;
+	i++;
+	if(sayi>10 &&sayi<200){
+	  int x=3*(sayi+2);
+	  if (x%5==0){
+	  	cout<<sayi<<" Uygun Sayýdýr."<<endl;
+	  }
+	  else if (x%5!=0){
+        cout<<sayi<<" Uygun Sayi Deðildir.";
+	  }
+	}
+	else {
+		cout<<"Girdiðiniz Sayý Uygun Deðildir.";
+	}
+}
+}
