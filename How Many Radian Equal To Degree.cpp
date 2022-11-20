bd66130dd2006b49106d9b1a6bdf290bd0494cce
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2022-11-20 13:42:02 +0300

    Add files via upload

---
+++ How Many Radian Equal To Degree.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<locale.h>
+#include<conio.h>
+#include<unistd.h>
+#define pi 3.14
+template<typename t>
+
+t Convert(t sayi){
+	t angle=(pi/180)*sayi;
+	return angle/pi;
+}
+
+int main(void){
+	setlocale(LC_ALL,"Turkish");
+	printf("Welcome To Program...");
+	float result;
+	float sayi;
+	char sec,ch;
+    while(1){
+     printf("\nWrite Your Degree For Transforming Degree : ");
+     scanf("%f",&sayi);
+     result=Convert(sayi);
+     printf("\nValue Of %.2f Radian:%.4lf",result,result*pi);
+while(1){
+	
+printf("\nWould You Like To Contunie Program (Y/N) ? : ");
+ch=getche();
+if (ch=='Y'|| ch=='y'){
+	sleep(3);
+	break;
+}
+else if (ch=='N'|| ch=='n'){
+	printf("\nHave a Good Days...");
+	exit(1);
+}
+else{
+	printf("\nWrong Input...\a");
+}
+}
+}
+}
--- Show Local Time.cpp
+++ Show Local Time.cpp
@@ -3,7 +3,7 @@
 
 
 int main(void){
-	setlocale(LC_ALL,"Turkish");
+	
 	printf("Welcome To Hour Program...\n");
 	time_t time_1970;
 	time_1970=time('\0');
