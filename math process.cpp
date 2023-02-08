66c7cb0d49650ee9532faf96826c96a7e7c2296b
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2023-02-08 11:47:32 +0300

    Add files via upload

---
+++ math process.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<math.h>
+#include<time.h>
+#include<conio.h>
+#include<unistd.h>
+template<typename T>
+
+T Program(T a){
+	printf("\nSquare of Number: %.2f\nCubic of Number: %.2f ",pow(a,2),pow(a,3));
+	printf("\nSquare Root of Number: %.2f\nCubic Root of Number: %.2f",sqrt(a),cbrt(a));
+	
+}
+
+int main(){
+	double num;
+	char ch;
+	time_t Time;
+	Time=time('\0');
+	printf("Welcome To Program...\n%s",ctime(&Time));
+  while(1){
+	while(1){
+	  printf("\nWrite a Number: ");scanf("%lf",&num);
+	  if (num<0){
+		num=abs(num);
+	}
+	Program(num);
+	while(1){
+		printf("\nWould You Like To Contunie(Y/N) ? : ");ch=getche();
+		if (ch=='Y' || ch=='y'){
+			sleep(2);
+			break;
+		}
+		else if (ch=='N' || ch=='n'){
+			printf("\nHave a Good Days...");
+			exit(1);
+		}
+		else{
+			printf("\nWrong Character...\nPlease Write Again...\a");
+		}
+	}
+	}
+	
+}
+}
