2e761a8e16b0f96f77f502f7b2606f0328139f5c
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2023-03-22 09:48:33 +0300

    Add files via upload

---
+++ Code Which Finding The Biggest Order of Given Number.cpp
@@ -0,0 +1,81 @@
+﻿
+#include <iostream>
+#include<time.h>
+#include<Windows.h>
+#include<conio.h>
+#include<math.h>
+
+using namespace std;
+
+class Number {
+private:
+    int num, sayac;
+    int num2; bool state;
+public:
+    Number(int num,bool state){
+        this->num = num;
+        this->state = state;
+        num2 = num;
+        sayac = 0;
+        
+    }
+    int Rem() {
+        while (1) {
+            num2 /= 10;
+            sayac++;
+            if (num2 < 10 && num2 >= 0) {
+                
+                break;
+            }
+        }
+        return sayac;
+    }
+    void Process() {
+        if (state == true) {
+            cout << endl << sayac + 1 << "\033[1;40m. order Of " << num << " : " << (int)(num / (pow(10, Rem())));
+        }
+        else {
+            cout << endl << sayac + 1 << "\033[1;40m. order Of -" << num << " : " << (int)(num / (pow(10, Rem())));
+        }
+        
+    }
+
+    ~Number() {
+        cout << endl << "Process Could Be Finished Successfully";
+    }
+};
+
+//int Number::num2 =5;
+int main()
+{    
+    cout << endl << "\033[1;33mWelcome To Program...\n";
+    
+    while (1) {
+        int num; char ch[5]; bool state=true;
+        cout << endl << "\033[1;34mWrite a Number : \033[1;0m"; cin >> num; cin.ignore();
+        if (num < 0) {
+            num = abs(num);
+            state = false;
+        }
+        Number *number=new Number(num,state);
+        number->Process();
+        while (1) {
+            cout << endl << "\033[1;36mWould You Like To Contunie (Y/N) : "; cin.getline(ch, 5, 10); 
+            if (*ch == 'Y' || *ch == 'y' || ch == "Yes") {
+                Sleep(2000);
+                break;
+            }
+            else if (*ch == 'N' || *ch == 'n' || ch == "No" || ch == "no") {
+                printf("\n\033[1;35mHave a Good Days...\033[0m"); _exit(0);
+                delete number;
+            }
+            else {
+                cout << endl << "\033[1;31mWrong Character..." << endl << "Please Write Again...\a";
+            }
+
+        }
+    }
+
+    return 0;
+    
+}
