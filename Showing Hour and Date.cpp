2fe933564aa6a34af6acc37345b718d6af952729
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2023-04-19 22:32:00 +0300

    Update Showing Hour and Date.cpp

--- Showing Hour and Date.cpp
+++ Showing Hour and Date.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 int main(void){
 	time_t Time;
-	Time=time('\0');
+	Time=time(NULL);
 	struct tm *Tm=localtime(&Time);
-	cout<<endl<<"Localtime: "<<strftime()
+
 	cout<<"Welcome To Hour Program..."<<endl<<"Hour: "<<Tm->tm_hour<<" : "<<Tm->tm_min<<" : "<<Tm->tm_sec;
 	cout<<endl<<"Date: "<<Tm->tm_mday<<" / "<<Tm->tm_mon+1<<" / "<<Tm->tm_year+1900;
 	printf("\nToday is %d.Day Of This Year",Tm->tm_yday);
